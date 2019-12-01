90593cce1d3f13436bff0de402385d5c0d5dd07f
shkib <58365851+shkib@users.noreply.github.com>
2019-12-01 15:58:32 +0300

    Update shkib12.py

--- shkib12.py
+++ shkib12.py
@@ -8,7 +8,7 @@ def inline(message):
   key = types.InlineKeyboardMarkup()
   but_1 = types.InlineKeyboardButton(text="КОНЕЧНО!!!", callback_data="FirstOne")
   key.add(but_1)
-  bot.send_message(message.chat.id, "ЗДАРОВА, МЕНЯ ЗОВУТ БОТ ОЛЕГ !!! \n Хочешь я покажу тебе чему я пока научился? \n Садись поудобнее. Расслабься. \n Готов?", reply_markup=key)
+  bot.send_message(message.chat.id, "ЗДАРОВА, МЕНЯ ЗОВУТ ОЛЕГ !!! \n Хочешь я покажу тебе чему я пока научился? \n  \n Готов?", reply_markup=key)
 
 @bot.callback_query_handler(func=lambda c:True)
 def inline(c):
@@ -22,7 +22,7 @@ def inline(c):
       key = types.InlineKeyboardMarkup()
       but_2 = types.InlineKeyboardButton(text="ДАВАЙ!", url='https://t.me/MDKaka' )
       key.add(but_2 )
-      bot.send_audio(c.message.chat.id, 'https://i46.kissvk.com/api/song/download/get/10/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%B5%D0%BD-%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F-kissvk.com.mp3?origin=kissvk.com&url=sid%3A%2F%2F285402691_456239024_5ebe5da7ff842a52ad_8357ecb6cfb0ef7a3a&artist=%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%B5%D0%BD&title=%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F&index=0&future_urls=sid%3A%2F%2F285402691_456239023_9f9d967fe0414d6ddb_c4e04a2c46c5fbc739', caption='Вообще я люблю рэп. Но в последнее время я перестал уделять ему внимание. Надеюсь скоро мои настройки обновяться и снова смогу писать клевую музычку и читать классные тексты. Ну пока немного юмора!', reply_markup=key)
+      bot.send_audio(c.message.chat.id, 'https://i46.kissvk.com/api/song/download/get/10/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%B5%D0%BD-%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F-kissvk.com.mp3?origin=kissvk.com&url=sid%3A%2F%2F285402691_456239024_5ebe5da7ff842a52ad_8357ecb6cfb0ef7a3a&artist=%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%B5%D0%BD&title=%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F&index=0&future_urls=sid%3A%2F%2F285402691_456239023_9f9d967fe0414d6ddb_c4e04a2c46c5fbc739', caption='Вообще я люблю рэп. Но в последнее время я перестал уделять ему внимание. Надеюсь скоро мои настройки обновяться и снова смогу писать клевую музычку и читать классные тексты. Ну пока все. Глянь меню внизу!', reply_markup=markup_menu)
 
 
 
